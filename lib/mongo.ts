bc8eb51dd37c42bb46c0e56a820b752eeeb4fd7e
UNAINR <munain605@gmail.com>
2024-09-22 19:58:30 +0500

    vw

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -19,10 +19,11 @@ const dbConnect = async () => {
         return cached.conn;
     }
 
-    // If a connection does not exist, check if a promise is already in progress
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         };
 
         cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
@@ -40,4 +41,4 @@ const dbConnect = async () => {
     return cached.conn;
 }
 
-export default dbConnect; // Make sure to export dbConnect
+export default dbConnect;
