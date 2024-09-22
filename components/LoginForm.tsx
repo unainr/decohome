4ed27124ad0af7c4f70bcaacd075f6c2e89bf12e
UNAINR <munain605@gmail.com>
2024-09-22 21:53:51 +0500

    f

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
     setLoading(true);
 
     try {
-      const res = await fetch('/api/auth', {
+      const res = await fetch('https://decorcor.netlify.app/api/auth', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password, action: 'login' }),
