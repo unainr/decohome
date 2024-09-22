a98d0228b9879a5ede5356f7e902df9d6824d280
UNAINR <munain605@gmail.com>
2024-09-22 11:05:58 +0500

    file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Enable App Router
+    },
+  };
+  
+  export default nextConfig;
+  
