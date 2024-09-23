e9b2844612c0110743b46f85c5a5a3137157f379
UNAINR <munain605@gmail.com>
2024-09-23 20:07:27 +0500

    sswwwwww

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL:any = process.env.MONGODB_URL;
+const MONGODB_URL: string = process.env.MONGODB_URL || '';
 
 if (!MONGODB_URL) {
-    throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
+    throw new Error('Please define the MONGODB_URL environment variable in .env.local');
 }
 
 let cached = (global as any).mongoose;
 
 if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null };
+    cached =(global as any).mongoose = { conn: null, promise: null };
 }
 
 export async function dbConnect() {
@@ -20,14 +20,15 @@ export async function dbConnect() {
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URL, {
             bufferCommands: false,
-            maxPoolSize: 20, // Use larger pool size for better concurrency
-            socketTimeoutMS: 30000, // Set socket timeout
-            serverSelectionTimeoutMS: 5000, // Fail fast if server selection fails
+            maxPoolSize: 20,
+            socketTimeoutMS: 30000,
+            serverSelectionTimeoutMS: 5000,
         }).then((mongoose) => mongoose);
     }
 
     cached.conn = await cached.promise;
     return cached.conn;
 }
+
 
 export default dbConnect;
